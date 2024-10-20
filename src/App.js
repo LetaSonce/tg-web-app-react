283c87f6227b2bf4d30c2d21bf71428b630b1ae2
Leta Sonce <letasonce@gmail.com>
2024-10-20 23:24:37 +0300

    init: initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import WebApp from '@twa-dev/sdk';
+
 
 function App() {
+
+  useEffect(() => {
+    WebApp.ready()
+  }, [])
+
+  const onClose = () => {
+    WebApp.close()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClose={onClose}>X</button>
     </div>
   );
 }
